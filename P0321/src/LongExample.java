60a55ebb4006266fca2a55a719e02a24063497e1
21-manic-017g <21-manic-017g@21-manic-017>
2024-03-21 14:23:28 +0900

    Operator

---
+++ P0321/src/LongExample.java
@@ -0,0 +1,16 @@
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 200L;
+		long var3 = 100000000000;
+		long var4 = 100000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+
+	}
+
+}
