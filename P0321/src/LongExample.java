113d541bfd85c47f73f913f3b9187e99ea1d9c19
21-manic-017g <21-manic-017g@21-manic-017>
2024-03-21 14:29:06 +0900

    Java

--- P0321/src/LongExample.java
+++ P0321/src/LongExample.java
@@ -4,7 +4,7 @@ public class LongExample {
 	public static void main(String[] args) {
 		long var1 = 10;
 		long var2 = 200L;
-		long var3 = 100000000000;
+//		long var3 = 100000000000;
 		long var4 = 100000000000L;
 		
 		System.out.println(var1);
